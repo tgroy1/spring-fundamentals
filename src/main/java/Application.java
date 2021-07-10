f2822e4aa0c78b680877c770cf20a46bea62ab46
Tiyas <tgroy1@gmail.com>
2021-07-10 23:05:59 +0530

    findAll() called once instead of repeat calls

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,10 @@
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.tgroy.model.Speaker;
 import com.tgroy.service.SpeakerService;
 
 public class Application {
@@ -11,9 +14,11 @@ public class Application {
 		ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
 		SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
-
-		System.out.println("Name = " + service.findAll().get(0).getFirstName());
-		System.out.println("Seed = " + service.findAll().get(0).getSeed());
+		
+		List<Speaker> speakers = service.findAll();
+		
+		System.out.println("name: " + speakers.get(0).getFirstName());
+		System.out.println("seed: " + speakers.get(0).getSeed());
 
 		System.out.println("Ending the application now");
 		((ConfigurableApplicationContext) appContext).close();
